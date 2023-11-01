4ed05db4bde19ce2a925d94b5fac931700c93fd7
Mateo Garcia <mateo.garcia@evolvevacationrental.com>
2023-11-01 15:59:09 -0500

    feat #7: dockerize dev environment

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,43 @@
-import express from 'express'
-import cors from 'cors'
-import passport from 'passport'
-import { HealthcheckController } from './controllers/healthcheck.controller'
-import { UserController } from './controllers/user.controller'
-import { AuthController } from './controllers/auth.controller'
-import { db } from './lib/db'
-import { logger, requestsLogger } from './lib/logger'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import passport from "passport";
+import { HealthcheckController } from "./controllers/healthcheck.controller";
+import { UserController } from "./controllers/user.controller";
+import { AuthController } from "./controllers/auth.controller";
+import { db } from "./lib/db";
+import { logger, requestsLogger } from "./lib/logger";
 
-const port = process.env.PORT || 3001
-const app = express()
+const host = process.env?.SERVER_HOST || "0.0.0.0";
+const port = parseInt(process.env?.PORT as string) || 3000;
+const baseUrl = `http://${host}:port`;
+const app = express();
 
 db.initialize()
   .then(() => {
-    logger.info('database connection successfully')
+    logger.info("database connection successfully");
   })
-  .catch(() => {
-    logger.error('database connection error')
-  })
+  .catch((err) => {
+    logger.error(err);
+    logger.error("database connection error");
+  });
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: baseUrl,
     credentials: true,
   })
-)
-app.use(express.json())
-app.use(passport.initialize())
-app.use(requestsLogger)
+);
+app.use(express.json());
+app.use(passport.initialize());
+app.use(requestsLogger);
 
-logger.info('middlewares registered successfully')
+logger.info("middlewares registered successfully");
 
-app.use('/healthcheck', new HealthcheckController().getRouter())
-app.use('/users', new UserController().getRouter())
-app.use('/auth', new AuthController().getRouter())
+app.use("/healthcheck", new HealthcheckController().getRouter());
+app.use("/users", new UserController().getRouter());
+app.use("/auth", new AuthController().getRouter());
 
-app.listen(port, () => {
-  logger.info(`http server running on ${port}`)
-})
+app.listen(port, host, () => {
+  logger.info(`http server running on ${baseUrl}`);
+});
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,19 @@
-import { DataSource } from 'typeorm'
+import "dotenv/config";
+import { DataSource } from "typeorm";
 
 const db = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'god',
-  password: 'mode',
-  database: 'etm-db-dev',
+  type: "postgres",
+  host: process.env.POSTGRES_HOST ?? "localhost",
+  port: parseInt(process.env.POSTGRES_PORT ?? "5432"),
+  username: process.env.POSTGRES_USER ?? "",
+  password: process.env.POSTGRES_PASSWORD ?? "",
+  database: process.env.POSTGRES_DB ?? "",
   synchronize: true,
   logging: false,
-  entities: ['src/entities/*.ts'],
+  entities: ["src/entities/*.ts"],
   subscribers: [],
-  migrations: ['src/migrations/*.ts'],
-  schema: 'public',
-})
+  migrations: ["src/migrations/*.ts"],
+  schema: "public",
+});
 
-export { db }
+export { db };
